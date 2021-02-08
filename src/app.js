32ae97c5b579aa803230d2dfaecf36001924d32a
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-02-08 17:10:54 +0000

    v3 db.js logic

--- src/app.js
+++ src/app.js
@@ -1,31 +1,14 @@
-/*
-Keyrt með:
-node 03.xss-persistent.js
-
-Keyrir upp express þjón með formi á /
-Upplýsingar eru vistaðar í gagnagrunn án þess að strengur sé hreinsaður m.t.t.
-XSS (og annars!)
-Á /data eru allar færslur úr grunni sýndar og þar með möguleiki á að sýna gögn
-sem innihalda XSS.
-*/
-
-
 import pg from 'pg';
 import express from 'express';
 import xss from 'xss';
 import dotenv from 'dotenv';
-import {  }  from './db.js';
+import { insert, select }  from './db.js';
 
-dotenv.config();
+// dotenv.config();
 
-const {
-   PORT: port = 3000
+const {   
+  PORT: port = 3000,
 } = process.env;
-
-
-
-const connectionString = 'postgres://vef2-2021:123@localhost/vef2-2021-v2';
-const pool = new pg.Pool({ connectionString });
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -34,31 +17,6 @@ app.use(express.static('public'));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
-
-async function insert(name, nationalId, comment, anonymous) {
-  const client = await pool.connect();
-  try {    
-    await client.query('INSERT INTO signatures(name, nationalId, comment, anonymous) VALUES ($1,$2,$3,$4)', [name, nationalId, comment, anonymous]);
-  } catch (e) {
-    console.error('Error', e);
-  } finally {
-    client.release();
-  }
-}
-
-async function select() {
-  const client = await pool.connect();
-
-  try {
-    const res = await client.query('SELECT * FROM signatures');
-    return res.rows;
-  } catch (e) {
-    console.error('Error selecting', e);
-  } finally {
-    client.release();
-  }
-  return [];
-}
 
 function dateFormat(data) {
   let datesArr = []
--- src/db.js
+++ src/db.js
@@ -3,15 +3,41 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// const connectionString = 'postgres://vef2-2021:123@localhost/vef2-2021-v2';
 
-// console.log(DATABASE_URL);
 const {  
   DATABASE_URL: connectionString,
 } = process.env;
 
-// console.log(process.env);
 
 if (!connectionString) {
   console.error('Vantar DATABASE_URL');
   process.exit(1);
+}
+
+const pool = new pg.Pool({ connectionString });
+
+export async function insert(name, nationalId, comment, anonymous) {
+  const client = await pool.connect();
+  try {    
+    await client.query('INSERT INTO signatures(name, nationalId, comment, anonymous) VALUES ($1,$2,$3,$4)', [name, nationalId, comment, anonymous]);
+  } catch (e) {
+    console.error('Error', e);
+  } finally {
+    client.release();
+  }
+}
+
+export async function select() {
+  const client = await pool.connect();
+
+  try {
+    const res = await client.query('SELECT * FROM signatures');
+    return res.rows;
+  } catch (e) {
+    console.error('Error selecting', e);
+  } finally {
+    client.release();
+  }
+  return [];
 }
