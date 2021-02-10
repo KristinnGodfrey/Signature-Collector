a1a37368854f9be989629a2b4dbf6c268e5a7eb4
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-02-10 16:16:31 +0000

    v5 b4 feature registration and proper routing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import xss from 'xss';
-import { insert, select }  from './db.js';
-import { body, sanitize, validationResult } from 'express-validator';
+import { body, validationResult } from 'express-validator';
+import { insert, select } from './db.js';
 
-const {   
-  PORT: port = 3000,
-} = process.env;
+const { PORT: port = 3000 } = process.env;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -17,102 +15,98 @@ app.set('view engine', 'ejs');
 
 const nationalIdPattern = '^[0-9]{6}-?[0-9]{4}$';
 
-let dateFormat = (data) => {
-  let datesArr = []
-  data.forEach(d => {
-    let year = d.signed.getFullYear();
-    let month = d.signed.getMonth();
-    let day = d.signed.getDate();
+const dateFormat = (data) => {
+  const datesArr = [];
+  data.forEach((d) => {
+    const year = d.signed.getFullYear();
+    const month = d.signed.getMonth();
+    const day = d.signed.getDate();
     datesArr.push(`${day}, ${month}, ${year}`);
-  })
+  });
   return datesArr;
-};
-
-let checkFormErrs = (name, nationalId, comment) => {
-  const nationalIdPattern = '^[0-9]{6}-?[0-9]{4}$';
-  let formErrs = {};
-  let nameBool;
-  let nationalIdBool;
-  let commentBool;
-  
-  if (name.length == 0) {
-    nameBool = false;
-    formErrs.push(nameBool);
-  }
-
-  if (nationalId.length != 10) {
-    
-    nationalIdBool = false;
-  }
-
-  return formErrs;
 };
 
 app.get('/', async (req, res) => {
   const data = await select();
 
-  let formattedDates = dateFormat(data);
-  let errorMessages = {};
-  let errorTypes = {};
-   
-  
-  let empty;
-  if (data.length == 0) {
-    res.render('index', {empty: "Engar undirskriftir.", formattedDates: formattedDates, errorMessages: errorMessages, errorTypes: errorTypes});
+  const formattedDates = dateFormat(data);
+  const errorMessages = {};
+  const errorTypes = {};
+
+  if (data.length === 0) {
+    res.render('index', {
+      empty: 'Engar undirskriftir.',
+      formattedDates,
+      errorMessages,
+      errorTypes,
+    });
   }
-  res.render('index', {data: data, empty: false, formattedDates: formattedDates, errorMessages: errorMessages, errorTypes: errorTypes});
+  res.render('index', {
+    data,
+    empty: false,
+    formattedDates,
+    errorMessages,
+    errorTypes,
+  });
 });
 
 app.post(
-  '/', 
+  '/',
   // validation
-  body('name')
-    .isLength({ min: 1 })
-    .withMessage('Nafn má ekki vera tómt'),
+  body('name').isLength({ min: 1 }).withMessage('Nafn má ekki vera tómt'),
   body('nationalId')
     .isLength({ min: 1 })
     .withMessage('Kennitala má ekki vera tóm'),
   body('nationalId')
     .matches(new RegExp(nationalIdPattern))
     .withMessage('Kennitala verður að vera á formi 000000-0000 eða 0000000000'),
-  
-  async (req, res, next) => { 
+
+  async (req, res, next) => {
     const errors = validationResult(req);
-    
-    
+
     if (!errors.isEmpty()) {
-      const errorMessages = errors.array().map(i => i.msg);
-      const errorTypes = errors.array().map(i => i.param);
+      const errorMessages = errors.array().map((i) => i.msg);
+      const errorTypes = errors.array().map((i) => i.param);
 
-      const data = await select();      
-      let formattedDates = dateFormat(data);
+      const data = await select();
+      const formattedDates = dateFormat(data);
 
-      return res.render('index', {data: data, empty: false, formattedDates: formattedDates, errorMessages: errorMessages, errorTypes: errorTypes});
+      return res.render('index', {
+        data,
+        empty: false,
+        formattedDates,
+        errorMessages,
+        errorTypes,
+      });
     }
 
-    next();
-    
+    return next();
   },
   // sanitation
   body('name').trim().escape(),
   body('nationalId').blacklist('-'),
 
-  async (req, res) => { 
-    let {name, nationalId, comment} = req.body;
-    let anonymous = req.body.anonymous;
-    if(anonymous == "on") anonymous = true;
+  async (req, res) => {
+    const { name, nationalId, comment } = xss(req.body);
+    let anonymous = xss(req.body.anonymous);
+    if (anonymous === 'on') anonymous = true;
     else anonymous = false;
 
     await insert(name, nationalId, comment, anonymous);
 
     const data = await select();
-    let formattedDates = dateFormat(data);
-    let errorMessages = {};
-    let errorTypes = {};
+    const formattedDates = dateFormat(data);
+    const errorMessages = {};
+    const errorTypes = {};
 
-    return res.render('index', {data: data, empty: false, formattedDates: formattedDates, errorMessages: errorMessages, errorTypes: errorTypes });
-  }
-
+    return res.render('index', {
+      data,
+      empty: false,
+      formattedDates,
+      errorMessages,
+      errorTypes,
+    });
+  },
 );
 
 app.listen(port, () => {
--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const {  
+const {
   DATABASE_URL: connectionString,
 } = process.env;
 
@@ -12,11 +12,11 @@ if (!connectionString) {
   process.exit(1);
 }
 
-const pool = new pg.Pool({ connectionString /*, ssl: { rejectUnauthorized: false}*/ });
+const pool = new pg.Pool({ connectionString /* , ssl: { rejectUnauthorized: false} */ });
 
 export async function insert(name, nationalId, comment, anonymous) {
   const client = await pool.connect();
-  try {    
+  try {
     await client.query('INSERT INTO signatures(name, nationalId, comment, anonymous) VALUES ($1,$2,$3,$4)', [name, nationalId, comment, anonymous]);
   } catch (e) {
     console.error('Error', e);
